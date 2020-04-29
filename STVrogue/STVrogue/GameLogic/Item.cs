048ef3694e05e9067806f8ac5eb0b2cb2c844ef6
Max <maxvangogh@home.nl>
2020-04-29 12:55:03 +0200

    Add Item class

--- STVrogue/STVrogue/GameLogic/Item.cs
+++ STVrogue/STVrogue/GameLogic/Item.cs
@@ -5,6 +5,7 @@ namespace STVrogue.GameLogic
     {
         public Item(String ID) : base(ID){ }
         
+        public virtual void Use(Player player){ }
     }
 
     public class HealingPotion : Item
@@ -18,13 +19,52 @@ namespace STVrogue.GameLogic
         }
         
         public int HealValue1 => healValue;
-
+        
+        /// <summary>
+        /// Use the healing potion, adding it's healValue to the players' HP
+        /// </summary>
+        /// <param name="player">The player object</param>
+        public override void Use(Player player)
+        {
+            // Check if the player actually has this item in their bag
+            if (player.Bag.Contains(this))
+            {
+                // Record the old hp
+                int prevHP = player.Hp;
+                
+                // Heal the player
+                player.Hp += this.healValue;
+                
+                // Remove this item from the players' bag
+                player.Bag.Remove(this);
+                
+                // TODO: write to the console that this potion was used
+            }
+        }
     }
 
     public class RagePotion : Item
     {
         public RagePotion(String ID) : base(ID){ }
         
+        /// <summary>
+        /// Use the rage potion, enraging the player
+        /// </summary>
+        /// <param name="player">The player object</param>
+        public override void Use(Player player)
+        {
+            // Check if the player actually has this item in their bag
+            if (player.Bag.Contains(this))
+            {
+                // Enrage the player
+                player.Enraged = true;
+                
+                // Remove this item from the players' bag
+                player.Bag.Remove(this);
+                
+                // TODO: write to the console that this potion was used
+            }
+        }
     }
 
 }
