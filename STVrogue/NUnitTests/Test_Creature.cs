6df9200db2902da44ca78bf24d630fbde5577073
Geert Jansen <geert.jansen95@hotmail.com>
2020-04-29 15:03:14 +0200

    Added start of Test player and monster

---
+++ STVrogue/NUnitTests/Test_Creature.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using NUnit.Framework;
+using STVrogue.GameLogic;
+
+namespace NUnitTests
+{
+    // Tests for the creature class
+    [TestFixture]
+    public class Test_Creature
+    {
+        // Test monster move to neighboring room
+        [Test]
+        public void Test_MonsterMove1()
+        {
+            Game game = new Game();
+        }
+        
+        // Test monster move to non neighboring room
+        // Test monster move to neighboring room with max capacity
+        
+        // Test monster attack decreasing player hp
+        // Test monster attack killing player 
+        
+        // Test player move to neighboring room
+        // Test player move to non neighboring room
+
+        // Test player attack decreasing monster hp
+        // Test player attack killing monster
+    }
+}
