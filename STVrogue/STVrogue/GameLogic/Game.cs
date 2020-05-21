9073f1713854f5998e8b5ce0b71948fd5819e3e5
Max <maxvangogh@home.nl>
2020-05-21 15:04:31 +0200

    Implemented Game.Update

--- STVrogue/STVrogue/GameLogic/Game.cs
+++ STVrogue/STVrogue/GameLogic/Game.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Reflection.Metadata;
 using STVrogue.GameControl;
 using static STVrogue.Utils.RandomFactory;
 
@@ -30,7 +31,7 @@ namespace STVrogue.GameLogic
             {
                 case DifficultyMode.NEWBIEmode:
                 {
-                    returnVal =  3;
+                    returnVal = 3;
                     break;
                 }
                 case DifficultyMode.NORMALmode:
@@ -48,14 +49,15 @@ namespace STVrogue.GameLogic
             return returnVal;
         }
     }
-    
+
     [Serializable()]
-    public enum DifficultyMode {
+    public enum DifficultyMode
+    {
         NEWBIEmode,
         NORMALmode,
         ELITEmode
     }
-    
+
     /// <summary>
     /// This class implements the logic of STV-Rogue. It holds its entire game-state
     /// and provides an update-method which is invoked every turn to execute all
@@ -71,7 +73,7 @@ namespace STVrogue.GameLogic
         DifficultyMode difficultyMode;
         bool gameover = false;
         private HashSet<Monster> livingMonsters;
-        
+
         /// <summary>
         /// Ignore this variable. It is added for some debug purpose.
         /// </summary>
@@ -82,12 +84,12 @@ namespace STVrogue.GameLogic
 
         int healUsed; // Keeps track of the last turn a heal potion was used
         int rageUsed; // Keeps track of the last turn a rage potion was used
-        
+
         public Game()
-        { 
-            //player = new Player("0", "Bagginssess");
+        {
+
         }
-        
+
         /// <summary>
         /// Try to create an instance of Game satisfying the specified configuration.
         /// It should throw an exception if it does not manage to generate a dungeon
@@ -97,16 +99,16 @@ namespace STVrogue.GameLogic
         {
             // Set the difficulty mode
             difficultyMode = conf.difficultyMode;
-            
+
             // Get the difficulty HP and AR multiplier
-            int difMult = conf.GetDifficultyMultiplier(difficultyMode);
-            
+            var difMult = conf.GetDifficultyMultiplier(difficultyMode);
+
             // Initialize the player
             player = new Player("playerId", "player", (conf.playerBaseHp * difMult), (conf.playerBaseAr * difMult));
-            
+
             // Initialize the dungeon
             dungeon = new Dungeon(conf.dungeonShape, conf.numberOfRooms, conf.maxRoomCapacity);
-            
+
             // Seed the monsters and items
             if (!dungeon.SeedMonstersAndItems(conf.initialNumberOfMonsters, conf.initialNumberOfHealingPots,
                 conf.initialNumberOfRagePots))
@@ -114,10 +116,10 @@ namespace STVrogue.GameLogic
                 // Throw and argument exception if this seeding fails
                 throw new ArgumentException("Could not seed the dungeon with the given parameters");
             }
-            
+
             // Get the list of all monsters created in the dungeon and store it
-            livingMonsters = dungeon.monsters;
-            
+            livingMonsters = new HashSet<Monster>();//dungeon.monsters;
+
             // Place the player in the starting room of the dungeon
             player.Location = dungeon.StartRoom;
         }
@@ -137,7 +139,7 @@ namespace STVrogue.GameLogic
             get => healUsed;
             set => healUsed = value;
         }
-        
+
         public int RageUsed
         {
             get => rageUsed;
@@ -145,7 +147,7 @@ namespace STVrogue.GameLogic
         }
 
         public bool Gameover => gameover;
-        
+
         public DifficultyMode DifficultyMode => difficultyMode;
 
         /// <summary>
@@ -161,7 +163,8 @@ namespace STVrogue.GameLogic
             }
             else
             {
-                throw new ArgumentException($"Destination ({destination.Id}) was not a neighbour to creature's location ({c.Location.Id})");
+                throw new ArgumentException(
+                    $"Destination ({destination.Id}) was not a neighbour to creature's location ({c.Location.Id})");
             }
         }
 
@@ -177,7 +180,8 @@ namespace STVrogue.GameLogic
             }
             else if (attacker.Location != defender.Location)
             {
-                throw new ArgumentException($"Attacker's ({attacker.Id}) location ({attacker.Location.Id}) does not match defender's ({defender.Id}) location ({defender.Location.Id})");
+                throw new ArgumentException(
+                    $"Attacker's ({attacker.Id}) location ({attacker.Location.Id}) does not match defender's ({defender.Id}) location ({defender.Location.Id})");
             }
             else
             {
@@ -193,7 +197,7 @@ namespace STVrogue.GameLogic
         {
             // Use the item
             player.Use(i);
-            
+
             if (i is HealingPotion)
             {
                 // Store the turn in which the item was used
@@ -203,7 +207,7 @@ namespace STVrogue.GameLogic
             {
                 // Store the turn in which the item was used
                 rageUsed = turnNumber;
-                
+
                 // Check the difficulty 
                 if (difficultyMode == DifficultyMode.ELITEmode)
                 {
@@ -224,9 +228,9 @@ namespace STVrogue.GameLogic
         /// </summary>
         public bool Flee(Creature c)
         {
-            List<Room> possibleRooms = c.Location.Neighbors.ToList();
-            bool canFlee = true;
-            
+            var possibleRooms = c.Location.Neighbors.ToList();
+            var canFlee = true;
+
             // Conditions for a monster
             if (c is Monster)
             {
@@ -242,11 +246,12 @@ namespace STVrogue.GameLogic
                 if(c.Location.Monsters.Count < 1) throw new ArgumentException($"Player: {c.Id} is not in combat");
                 
                 // In normal mode and elite mode: if player is enraged, player cannot flee
-                if ((DifficultyMode == DifficultyMode.NORMALmode || DifficultyMode == DifficultyMode.ELITEmode) && (c as Player).Enraged) canFlee = false;
-                
+                if ((DifficultyMode == DifficultyMode.NORMALmode || DifficultyMode == DifficultyMode.ELITEmode) &&
+                    (c as Player).Enraged) canFlee = false;
+
                 // In Elite mode: if player.eliteFlee is false, player cannot flee
                 if (DifficultyMode == DifficultyMode.ELITEmode && !(c as Player).EliteFlee) canFlee = false;
-                
+
                 // If heal potion is used at turn t, player can only flee at turn t+2 or later
                 if (TurnNumber <= HealUsed + 1) canFlee = false;
 
@@ -260,9 +265,10 @@ namespace STVrogue.GameLogic
                 }
                 
             }
+
             // if there is less then 1 possible room creature cannot flee
             if (possibleRooms.Count < 1) canFlee = false;
-            
+
             // If all conditions are met the creature flees to a random room in possibleRooms
             if (canFlee)
             {
@@ -282,65 +288,236 @@ namespace STVrogue.GameLogic
         public void Update(Command playerAction)
         {
             Console.WriteLine($"** Turn {turnNumber.ToString()}: {player.Name} {playerAction}");
-            
+
+            // Handle the given player action
+            HandlePlayerTurn(playerAction);
+
+            // Handle the monsters' turns
+            foreach (var m in livingMonsters)
+            {
+                HandleMonsterTurn(m);
+            }
+
+            // Update the turn number
+            turnNumber++;
+        }
+
+        /// <summary>
+        /// Handles the player's turn, depending on the given action
+        /// </summary>
+        /// <param name="playerAction">The command given by the player</param>
+        private void HandlePlayerTurn(Command playerAction)
+        {
             // Handle the given player action
             switch (playerAction.Name)
             {
                 // Handle the player doing nothing
                 case (CommandType.DoNOTHING):
                 {
-                    // TODO: handle the player doing nothing
-                    Console.WriteLine("      Lazy. Start working!");
+                    Console.WriteLine("You chose to do nothing.");
                     break;
                 }
                 // Handle the player moving to the given location
                 case (CommandType.MOVE):
                 {
-                    // TODO: handle the player moving to the given location
+                    HandlePlayerTurnMove(playerAction.Args[1]);
                     break;
                 }
                 // Handle the player attacking the given enemy
                 case (CommandType.ATTACK):
                 {
-                    // TODO: handle the player attacking the given enemy
-                    Console.WriteLine("      Clang! Wooosh. WHACK!");
+                    HandlePlayerTurnAttack(playerAction.Args[1]);
                     break;
                 }
                 // Handle the player picking up all the items in the room
                 case (CommandType.PICKUP):
                 {
-                    // TODO: handle the player picking up all items in his location
+                    // Add all items in the room to the player's bag
+                    foreach (var i in player.Location.Items)
+                    {
+                        player.Bag.Add(i);
+                    }
+                    // Remove all the items from the room
+                    player.Location.Items.Clear();
                     break;
                 }
                 // Handle the player using the given item
                 case (CommandType.USE):
                 {
-                    // TODO: handle the player using the given item
+                    HandlePlayerTurnUse(playerAction.Args[1]);
                     break;
                 }
                 // Handle the player fleeing
                 case (CommandType.FLEE):
                 {
-                    // TODO: handle the player fleeing
-                    Console.WriteLine("      We knew you are a coward.");
+                    // Check if the player can flee, executing the action if possible
+                    if (Flee(player))
+                    {
+                        Console.WriteLine("You have successfully fled from the fight");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your attempt to flee from combat was not successful");
+                    }
                     break;
                 }
             }
-            
-            // Handle the monsters' turns
-            foreach (Monster m in livingMonsters)
-            {
-                int actionMax = 2;
-                if (m.Location == player.Location)
-                {
-                    actionMax += 2;
-                }
-                int action = 
-            }
-            
-            // Update the turn number
-            turnNumber++;
         }
-        
+
+        /// <summary>
+        /// Handles the player's move command
+        /// </summary>
+        /// <param name="targetId">The id of the room the player wants to move to</param>
+        private void HandlePlayerTurnMove(string targetId)
+        {
+            // Create a bool denoting if the room was found
+            var found = false;
+            // Find the room with the correct ID
+            foreach (var r in player.Location.Neighbors)
+            {
+                // When found, move to that location
+                if (r.Id == targetId)
+                {
+                    Move(player, r);
+                    found = true;
+                    Console.WriteLine($"You moved to room {targetId}");
+                }
+            }
+            // if the destination was not found, throw an exception
+            if (!found)
+            {
+                throw new ArgumentException($"The given room ({targetId}) was not accessible from the player's location ({player.Location.Id})");
+            }
+        }
+
+        /// <summary>
+        /// Handles the player's attack command
+        /// </summary>
+        /// <param name="targetId">The id of the monster the player wants to attack</param>
+        private void HandlePlayerTurnAttack(string targetId)
+        {
+            // Create a bool denoting if the target was found
+            var found = false;
+            // Find the monster with the correct ID
+            foreach (var m in player.Location.Monsters)
+            {
+                // When found, attack that monster
+                if (m.Id == targetId)
+                {
+                    // Store the monster's old HP
+                    int prevHp = m.Hp;
+                            
+                    // Attack the monster
+                    Attack(player, m);
+                    found = true;
+                    Console.WriteLine($"You attacked monster {targetId}, dealing {(prevHp - m.Hp).ToString()} damage");
+                }
+            }
+            // if the target was not found, throw an exception
+            if (!found)
+            {
+                throw new ArgumentException($"The given monster ({targetId}) was not in the same location as the player ({player.Location.Id})");
+            }
+        }
+
+        /// <summary>
+        /// Handles the player's use command
+        /// </summary>
+        /// <param name="targetId">The id of the potion the player wants to use</param>
+        private void HandlePlayerTurnUse(string targetId)
+        {
+            // Create a bool denoting if the item was found
+            var found = false;
+            // Find the item with the correct ID
+            foreach (var item in player.Bag)
+            {
+                // When found, use that item
+                if (item.Id == targetId)
+                {
+                    UseItem(item);
+                    found = true;
+                }
+            }
+            // if the target was not found, throw an exception
+            if (!found)
+            {
+                throw new ArgumentException($"The given item ({targetId}) was not in the player's bag");
+            }
+        }
+
+        /// <summary>
+        /// Handles the turn for the given monster
+        /// </summary>
+        /// <param name="m">The monster whose turn should be handled</param>
+        private void HandleMonsterTurn(Monster m)
+        {
+            // Decide how many actions the monster can choose from
+            int action;
+            // If he is in combat: do nothing, attack and flee
+            if (m.Location == player.Location)
+            {
+                // Set the action to either 1, 2 or 3
+                action = GetRandom().Next(1, 4);
+            }
+            // if he is not in combat: move or do nothing
+            else
+            {
+                // Set the action to either 0 or 1
+                action = GetRandom().Next(0, 2);
+            }
+
+            // Handle the chosen action
+            switch (action)
+            {
+                case 0: // move
+                {
+                    // Define all possible destinations
+                    var destinations = new List<Room>();
+
+                    // Check if there are any rooms the monster can move to
+                    foreach (var r in m.Location.Neighbors)
+                    {
+                        // Check if the room can still hold a monster
+                        if (r.Monsters.Count < r.Capacity)
+                        {
+                            destinations.Add(r);
+                        }
+                    }
+
+                    // Check if the monster can move to any room
+                    if (destinations.Count > 0)
+                    {
+                        // move to a random room
+                        Move(m, destinations[GetRandom().Next(0, destinations.Count)]);
+                    }
+
+                    // Else do nothing
+                    break;
+                }
+                case 1: // do nothing
+                {
+                    break;
+                }
+                case 2: // attack
+                {
+                    // Store the player's old HP
+                    int prevHp = player.Hp;
+                    
+                    // Handle the monster attacking the player
+                    Attack(m, player);
+                    
+                    Console.WriteLine($"Monster {m.Id} attacked you and dealt {(prevHp - player.Hp).ToString()} damage");
+                    break;
+                }
+                case 3: // flee
+                {
+                    // Handle the monster fleeing, it doesn't matter if the flee was successful because the monster does nothing if he can't flee.
+                    Flee(m);
+                    
+                    Console.WriteLine($"Monster {m.Id} fled from the fight");
+                    break;
+                }
+            }
+        }
     }
 }
