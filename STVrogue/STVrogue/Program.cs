0d3ee9492e8cc2d5543a257576fb489d35b5da19
Max <maxvangogh@home.nl>
2020-05-22 00:00:27 +0200

    Implement program

--- STVrogue/STVrogue/Program.cs
+++ STVrogue/STVrogue/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Text;
 using STVrogue.GameControl;
 using STVrogue.GameLogic;
@@ -12,6 +11,20 @@ namespace STVrogue
     /// </summary>
     class Program
     {
+        // A struct denoting the player's possible actions this turn
+        private struct PossibleActions
+        {
+            public bool CanQuit { get; set; }
+            public bool CanDoNothing { get; set; }
+            public bool CanMove { get; set; }
+            public bool CanAttack { get; set; }
+            public bool CanPickup { get; set; }
+            public bool CanUse { get; set; }
+            public bool CanFlee { get; set; }
+        }
+
+        private static PossibleActions _possibleActions;
+        
         // The game configuration for this instance
         private static GameConfiguration _gameConfiguration = new GameConfiguration();
 
@@ -35,42 +48,357 @@ namespace STVrogue
             InitGameConfig();
             game = new Game(_gameConfiguration);
             bool gameover = false;
-            while (!gameover || !game.Gameover)
+            while (!gameover && !game.Gameover)
             {
-                Console.WriteLine("You are in a room. It is dark, and it feels dangerous...");
-                Console.WriteLine("Your action: Move(m)   | Pick-item(p)  | Do-nothing(SPACE) | Quit(q)");
-                Console.Write("             Attack(a) |    Flee(f)    | Use-item(u) ");
-                var c = Console.ReadKey().KeyChar;
-                Console.WriteLine("");
-                switch (c)
+                // Check which actions the player can take
+                CheckPossibleActions();
+
+                sb.Clear();
+                // Fill the stringbuilder with some fitting flavortext
+                
+                sb.Append($"** HP: ").Append(game.Player.Hp).Append("/").Append(game.Player.HpMax).Append(", location: ").Append(game.Player.Location.Id).Append("\n");
+                sb.Append("You enter a cold and damp room ");
+                switch (_gameConfiguration.difficultyMode)
                 {
-                    case 'm':
-                        game.Update(new Command(CommandType.MOVE, ""));
+                    case DifficultyMode.NEWBIEmode:
+                        sb.Append("and you see ");
                         break;
-                    case 'a':
-                        game.Update(new Command(CommandType.ATTACK, ""));
+                    case DifficultyMode.NORMALmode:
+                        sb.Append("and with the light from your torch you see ");
                         break;
-                    case 'u':
-                        game.Update(new Command(CommandType.USE, ""));
+                    case DifficultyMode.ELITEmode:
+                        sb.Append("and in the darkness you can barely make out ");
                         break;
-                    case 'f':
+                }
+
+                if (_possibleActions.CanMove) // canMove means no combat
+                {
+                    sb.Append("an empty room");
+                    if (_possibleActions.CanPickup)
+                        sb.Append($" with {game.Player.Location.Items.Count.ToString()} items laying on the ground");
+                    sb.Append(".\n");
+                }
+                else
+                {
+                    sb.Append($"{game.Player.Location.Monsters.Count.ToString()} horrible monsters coming towards you");
+                    if (_possibleActions.CanPickup)
+                        sb.Append(
+                            $" with {game.Player.Location.Items.Count.ToString()} items laying on the ground behind them");
+                    sb.Append(".\n");
+                }
+
+                sb.Append("What do you do?").Append("\n");
+                Console.WriteLine(sb.ToString());
+                sb.Clear();
+
+                // Print the options
+                PrintPossibleActions();
+
+                // Read the input
+                var input = ReadValidInput();
+
+                switch (input)
+                {
+                    case 'm': // Move
+                        PrintMoveTargets();
+                        game.Update(new Command(CommandType.MOVE, GetMoveTarget()));
+                        break;
+                    case 'a': // Attack
+                        PrintAttackTargets();
+                        game.Update(new Command(CommandType.ATTACK, GetAttackTarget()));
+                        break;
+                    case 'u': // Use
+                        PrintUseTargets();
+                        game.Update(new Command(CommandType.USE, GetUseTarget()));
+                        break;
+                    case 'p': // Pickup
+                        game.Update(new Command(CommandType.PICKUP, ""));
+                        break;
+                    case 'f': // Flee
                         game.Update(new Command(CommandType.FLEE, ""));
                         break;
-                    case ' ':
+                    case ' ': // Do nothing
                         game.Update(new Command(CommandType.DoNOTHING, ""));
                         break;
-                    case 'q':
+                    case 'q': // Quit
                         gameover = true;
                         break;
                 }
             }
 
-            Console.WriteLine("** YOU WIN! Score:" + game.Player.Kp + ". Go ahead and brag it out.");
+            sb.Append("\n");
+            if (game.Gameover && game.Player.Location == game.Dungeon.ExitRoom)
+            {
+                sb.Append("You run through the door and are greeted with mountains of treasure in front of you").Append("\n");
+                sb.Append("Next to the seemingly endless piles of gold and silver you spot some old, but beautiful and powerful, weapons and armor.").Append("\n");
+                sb.Append("As you stuff your pockets with loot you see a small hatch leading outside").Append("\n");
+                sb.Append(
+                        "After you've left, loot in hand, standing in the fresh breeze, you hear that old familiar voice echo from the mountains again:")
+                    .Append("\n");
+                sb.Append($"\"Good job, {_gameConfiguration.playerName}. You have found the treasure of the ancients, may it serve you well\"").Append("\n");
+                sb.Append($"Score: {game.Player.Kp.ToString()}");
+            }
+            else
+            {
+                sb.Append($"As you fall to the ground, you feel your life slip away until everything fades to black").Append("\n");
+                sb.Append($"Your score: {game.Player.Kp.ToString()}");
+            }
+            Console.WriteLine(sb.ToString());
+            Console.ReadLine();
         }
 
+        /// <summary>
+        /// Find a valid item to use
+        /// </summary>
+        /// <returns>The id of the item the player wants to use</returns>
+        private static string GetUseTarget()
+        {
+            while (true)
+            {
+                var target = Console.ReadLine();
+                foreach (var i in game.Player.Bag)
+                {
+                    if (i.Id == target)
+                    {
+                        return target;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Print all items the player can use
+        /// </summary>
+        private static void PrintUseTargets()
+        {
+            sb.Append("\n| ");
+            foreach (var i in game.Player.Bag)
+            {
+                if (i is RagePotion)
+                    sb.Append("Rage Potion");
+                else
+                    sb.Append("Heal Potion");
+                sb.Append(" (").Append(i.Id).Append(") | ");
+            }
+            Console.WriteLine(sb.ToString());
+            sb.Clear();
+        }
+        /// <summary>
+        /// Find a valid attack target
+        /// </summary>
+        /// <returns>The id of the monster the player wants to attack</returns>
+        private static string GetAttackTarget()
+        {
+            while (true)
+            {
+                var target = Console.ReadLine();
+                foreach (var m in game.Player.Location.Monsters)
+                {
+                    if (m.Id == target)
+                    {
+                        return target;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Print all monsters the player can attack
+        /// </summary>
+        private static void PrintAttackTargets()
+        {
+            sb.Append("\n| ");
+            foreach (var m in game.Player.Location.Monsters)
+            {
+                sb.Append("monster (").Append(m.Id).Append(") | ");
+            }
+            Console.WriteLine(sb.ToString());
+            sb.Clear();
+        }
+
+        /// <summary>
+        /// Find a valid move target
+        /// </summary>
+        /// <returns>the id of the destination</returns>
+        private static string GetMoveTarget()
+        {
+            while (true)
+            {
+                var target = Console.ReadLine();
+                foreach (var r in game.Player.Location.Neighbors)
+                {
+                    if (r.Id == target)
+                    {
+                        return target;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Prints all rooms neighbouring the player's location
+        /// </summary>
+        private static void PrintMoveTargets()
+        {
+            sb.Append("\n| ");
+            foreach (var r in game.Player.Location.Neighbors)
+            {
+                if (r.RoomType == RoomType.ORDINARYroom)
+                {
+                    sb.Append("room  (").Append(r.Id).Append(") | ");
+                }
+                else if (r.RoomType == RoomType.STARTroom)
+                {
+                    sb.Append("start (").Append(r.Id).Append(") | ");
+                }
+                else if (r.RoomType == RoomType.EXITroom)
+                {
+                    sb.Append("exit  (").Append(r.Id).Append(") | ");
+                }
+            }
+            Console.WriteLine(sb.ToString());
+            sb.Clear();
+        }
+        
+        /// <summary>
+        /// Reads only a valid input key
+        /// </summary>
+        /// <returns>the valid input</returns>
+        private static char ReadValidInput()
+        {
+            var c = Console.ReadKey().KeyChar;
+            switch (c)
+            {
+                case 'm':
+                    if (_possibleActions.CanMove) return c;
+                    ReadValidInput();
+                    break;
+                case 'a':
+                    if (_possibleActions.CanAttack) return c;
+                    ReadValidInput();
+                    break;
+                case 'u':
+                    if (_possibleActions.CanUse) return c;
+                    ReadValidInput();
+                    break;
+                case 'f':
+                    if (_possibleActions.CanFlee) return c;
+                    ReadValidInput();
+                    break;
+                case 'p':
+                    if (_possibleActions.CanPickup) return c;
+                    ReadValidInput();
+                    break;
+                case ' ':
+                    if (_possibleActions.CanDoNothing) return c;
+                    ReadValidInput();
+                    break;
+                case 'q':
+                    if (_possibleActions.CanQuit) return c;
+                    ReadValidInput();
+                    break;
+                default:
+                    ReadValidInput();
+                    break;
+            }
+            return ' '; // Useless return statement, needed to make the compiler happy
+        }
+
+        /// <summary>
+        /// Print all actions the player can currently perform to the screen 
+        /// </summary>
+        private static void PrintPossibleActions()
+        {
+            int i = 0;
+            bool enterPossible = true;
+            if (_possibleActions.CanMove) // The player can move
+            {
+                sb.Append("| Move(m)           ");
+                i++;
+            }
+
+            if (_possibleActions.CanPickup) // The player can pickup
+            {
+                sb.Append("| Pickup items(p)   ");
+                i++;
+            }
+
+            if (_possibleActions.CanUse) // The player can use an item
+            {
+                sb.Append("| Use item(u)       ");
+                i++;
+            }
+
+            if (_possibleActions.CanAttack) // The player can attack
+            {
+                sb.Append("| Attack(a)         ");
+                i++;
+            }
+
+            if (i == 4)
+            {
+                sb.Append("\n");
+                enterPossible = false;
+            }
+
+            if (_possibleActions.CanFlee) // The player can flee
+            {
+                sb.Append("| Flee(f)           ");
+                i++;
+            }
+
+            if (i == 4 && enterPossible)
+            {
+                sb.Append("\n");
+                enterPossible = false;
+            }
+
+            if (_possibleActions.CanDoNothing) // The player can do nothing
+            {
+                sb.Append("| Do Nothing(SPACE) ");
+                i++;
+            }
+
+            if (i == 4 && enterPossible)
+            {
+                sb.Append("\n");
+                enterPossible = false;
+            }
+
+            if (_possibleActions.CanQuit) // The player can quit
+            {
+                sb.Append("| Quit(q)           ");
+                i++;
+            }
+
+            sb.Append("|\n");
+            Console.WriteLine(sb.ToString());
+            sb.Clear();
+        }
+
+        /// <summary>
+        /// Check which actions the player can take and store them in the PossibleMoves struct
+        /// </summary>
         private static void CheckPossibleActions()
         {
+            // If there are monsters at the player's location, he is in combat
+            // combat means attack and flee are possible, but no moving
+            _possibleActions.CanAttack = game.Player.Location.Monsters.Count > 0;
+            _possibleActions.CanFlee = game.Player.Location.Monsters.Count > 0;
+            // The player can also only move if there are neighbouring rooms to move to
+            _possibleActions.CanMove = game.Player.Location.Monsters.Count == 0 && game.Player.Location.Neighbors.Count > 0;
             
+            // Check if there are items in the player's location
+            // Items means pickup is possible
+            _possibleActions.CanPickup = game.Player.Location.Items.Count > 0;
+            
+            // Check if the player has any items
+            // Items mean use is possible
+            _possibleActions.CanUse = game.Player.Bag.Count > 0;
+            
+            // The player can always do nothing and quit
+            _possibleActions.CanDoNothing = true;
+            _possibleActions.CanQuit = true;
         }
 
         /// <summary>
@@ -194,10 +522,10 @@ namespace STVrogue
         /// </summary>
         private static void InitGameConfig()
         {
-            _gameConfiguration.numberOfRooms = GetRandom().Next(5, 15);
+            _gameConfiguration.numberOfRooms = 3; //GetRandom().Next(5, 15);
             _gameConfiguration.maxRoomCapacity = 10;
-            _gameConfiguration.dungeonShape = GetRandomDungeonShape();
-            _gameConfiguration.initialNumberOfMonsters = GetRandom().Next(15, 30);
+            _gameConfiguration.dungeonShape = DungeonShapeType.LINEARshape;//GetRandomDungeonShape();
+            _gameConfiguration.initialNumberOfMonsters = 1; //GetRandom().Next(15, 30);
             _gameConfiguration.initialNumberOfHealingPots = GetRandom().Next(1, 10);
             _gameConfiguration.initialNumberOfRagePots = GetRandom().Next(1, 10);
         }
