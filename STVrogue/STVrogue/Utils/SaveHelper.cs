f7293797683620065ddbe0299dbcbb13471d48fc
Max <maxvangogh@home.nl>
2020-06-16 15:33:32 +0200

    SaveHelper setup

---
+++ STVrogue/STVrogue/Utils/SaveHelper.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+
+namespace STVrogue.Utils
+{
+    public static class SaveHelper
+    {
+        private static StringBuilder sb;
+        private static string safeFileLocation = "";
+        
+        public 
+    }
+}
